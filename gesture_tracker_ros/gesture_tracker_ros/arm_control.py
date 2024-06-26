7c14568068522580d7c9489b2cde72bf9ac7114b
Eugene Frizza <eugenefrizza@gmail.com>
2024-06-25 22:57:35 -0700

    Working demo of hand teleop in XY

--- gesture_tracker_ros/gesture_tracker_ros/arm_control.py
+++ gesture_tracker_ros/gesture_tracker_ros/arm_control.py
@@ -8,26 +8,42 @@ class ArmControl(object):
         model_path = "/media/ssd/eugene/robotic_manipulation/models/lite6_viz.xml"
         self.model = mujoco.MjModel.from_xml_path(model_path)
         self.data = mujoco.MjData(self.model)
-        self.qcurr = None
-        #target_id = model.body('target').mocapid
-        self.target_pos = self.data.body('target').xpos
-        self.target_quat = self.data.body('target').xquat
+        # self.renderer = mujoco.Renderer(model, height=360, width=480)
+        # self.camera = mujoco.MjvCamera()
+        # mujoco.mjv_defaultFreeCamera(model, camera)
+        # camera.distance = 1.2
+        # camera.elevation = -15
+        # camera.azimuth = -130
+        # camera.lookat = (0, 0, 0.3)
+        # self.renderer = 
+        self.qcurr = np.zeros(self.model.nv)
+        self.mocapid = self.model.body('target').mocapid
+        self.target_pos = None
+        self.target_quat = None
+        self.sim = sim
         if not sim:
             self.arm = XArmAPI(arm_ip, is_radian=True)
             self.arm.set_mode(4)
-        self.Kp = 20
+            time.sleep(3)
+        self.Kp = 5
 
         self.pos_bounds = pos_bounds
         self.ref_site_id = self.data.site('attachment_site').id
         self.target_site_id = self.data.site('target').id
 
+        self.run = False
+
 
     def stop(self):
         self.arm.motion_enable(False)
+        self.run = False
 
     def start(self):
+        self.arm.set_state(state=0)
+        time.sleep(1)
         self.arm.motion_enable(True)
         time.sleep(1)
+        self.run = True
 
     def reset(self):
         self.arm.reset()
@@ -44,22 +60,12 @@ class ArmControl(object):
         
         # Pre-assign variables
         error = np.zeros(6)
-        #curr_quat = np.empty(4)
-        #target_quat = np.empty(4)
-        #curr_quat_conj = np.empty(4)
         mat_err = np.empty((3, 3))
         quat_err = np.empty(4)
-        dq = np.zeros(model.nv)
 
         # Pos error
         error[:3] = data.site(target_site_id).xpos - data.site(ref_site_id).xpos
-        print(data.site(target_site_id).xpos, data.site(ref_site_id).xpos)
         # Quat error
-        #mujoco.mju_mat2Quat(curr_quat, data.site(ref_site_id).xmat)
-        #mujoco.mju_mat2Quat(target_quat, data.site(target_site_id).xmat)
-        #mujoco.mju_negQuat(curr_quat_conj, curr_quat)
-        #mujoco.mju_mulQuat(quat_err, target_quat, curr_quat_conj)
-        
         mat_err = data.site(target_site_id).xmat.reshape((3, 3)) @ data.site(ref_site_id).xmat.reshape((3, 3)).T
         mujoco.mju_mat2Quat(quat_err, mat_err.reshape((9, 1)))
         mujoco.mju_quat2Vel(error[3:], quat_err, 1.0)
@@ -84,23 +90,24 @@ class ArmControl(object):
         2. Calculate dq
         3. Update vel
         """
-        if not self.target_pos:
+        if not self.run or self.target_pos is None or self.target_quat is None:
             return
         if self.arm.has_error:
-            print(f"Exiting, error {self.arm.error_code}")
-            return self.arm_error_code
+            #print(f"Exiting, error {self.arm.error_code}")
+            return self.arm.error_code
         
-        if not sim:
-            code, state = arm.get_servo_angle()
+        if not self.sim:
+            code, state = self.arm.get_servo_angle()
             self.qcurr = np.array(state[:6])
+            self.data.qpos = self.qcurr
 
-        self.data.qpos = self.qcurr
-        #self.data.body('target') = self.target_pose
+        self.data.mocap_pos[self.mocapid] = self.target_pos
+        self.data.mocap_quat[self.mocapid] = self.target_quat
         error = self.calculate_error(self.model, self.data, self.ref_site_id, self.target_site_id)
         dq = self.calculate_dq_damped(self.model, self.data, self.ref_site_id, error)
         
-        if not sim:
-            arm.vc_set_joint_velocity(speeds=list(Kp*dq), duration=0.1)
+        if not self.sim:
+            self.arm.vc_set_joint_velocity(speeds=list(self.Kp*dq), duration=0.1)
 
         return
 
--- gesture_tracker_ros/gesture_tracker_ros/gesture_tracker_node.py
+++ gesture_tracker_ros/gesture_tracker_ros/gesture_tracker_node.py
@@ -14,6 +14,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from geometry_msgs.msg import Point, PoseStamped
 
 from .arm_control import ArmControl
 
@@ -60,7 +61,7 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
 
 class GestureTrackerNode(Node):
-    def __init__(self, rate=10):
+    def __init__(self, camera_rate=10, control_rate=50):
         super().__init__('realsense_node')
         self.bridge = CvBridge()
 
@@ -77,47 +78,64 @@ class GestureTrackerNode(Node):
         self.pipeline.start(self.config)
         #self.pipeline.start()
 
-        # ROS2 publisher for image topic
-        self.image_pub = self.create_publisher(Image, 'camera/color/image_raw', 10)
-        self.timer = self.create_timer(1.0/rate, self.capture)  # Adjust timer based on desired frame rate
+        self.robot_control = ArmControl()
+        self.target_orientation = np.array([0., 1., 0., 0.]) # face down
 
-        # mediapipe setup
-        base_options = python.BaseOptions(model_asset_path='gesture_tracker_ros/resource/hand_landmarker.task')
+        # Publishers
+        self.image_pub = self.create_publisher(Image, 'camera/color/image_raw', 10)
+        self.hand_pos_pub = self.create_publisher(Point, '/hand/position', 5)
+        self.robot_pose_pub = self.create_publisher(PoseStamped, '/robot/pose', 5)
+        
+        # Robot pose msg
+        self.pose_msg = PoseStamped()
+        self.pose_msg.pose.orientation.w = self.target_orientation[0]
+        self.pose_msg.pose.orientation.x = self.target_orientation[1]
+        self.pose_msg.pose.orientation.y = self.target_orientation[2]
+        self.pose_msg.pose.orientation.z = self.target_orientation[3]
+        self.pose_msg.header.frame_id = 'attachment_site'
+        
+        # Mediapipe setup
+        base_options = python.BaseOptions(model_asset_path='src/gesture_tracker_ros/resource/hand_landmarker.task')
         options = vision.HandLandmarkerOptions(base_options=base_options,
                                        num_hands=1, running_mode=vision.RunningMode.LIVE_STREAM,
                                                result_callback=self.detection_cb)
         self.detector = vision.HandLandmarker.create_from_options(options)
         self.latest_detection_timestamp = None
+        self.latest_detection = None
 
-        arm_control = ArmControl()
-        arm_control.start()
+        self.timer = self.create_timer(1.0/camera_rate, self.capture)  
+        self.timer = self.create_timer(1.0/control_rate, self.robot_control.step)
+        self.robot_control.start()
         print("Node init finished")
-        #self.capture_and_publish()
 
     def capture(self):
-        #while rclpy.ok():
-            frames = self.pipeline.wait_for_frames()
-            color_frame = frames.get_color_frame()
-            if not color_frame:
-                print("no frame")
-                return
-                #continue
+        """
+        Capture a frame from the relasense
+        """
+        frames = self.pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        if not color_frame:
+            print("no frame")
+            return
 
-            timestamp = self.get_clock().now()
-            
-            if (self.latest_detection_timestamp is not None and self.latest_detection_timestamp.nanoseconds < timestamp.nanoseconds - 0.2*1e9):
-                print("lagging, skip frame")
-                # Convert image to OpenCV format
-            color_image = np.asanyarray(color_frame.get_data())
-            mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=color_image)
-            self.detector.detect_async(mp_img, int(timestamp.nanoseconds / 1e6))
-            
+        timestamp = self.get_clock().now()
+        
+        if (self.latest_detection_timestamp is not None and self.latest_detection_timestamp.nanoseconds < timestamp.nanoseconds - 0.2*1e9):
+            print("lagging, skip frame")
+            return
+        # Convert image to OpenCV format
+        color_image = np.asanyarray(color_frame.get_data())
+        mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=color_image)
+        self.detector.detect_async(mp_img, int(timestamp.nanoseconds / 1e6))
             
 
     def detection_cb(self, result: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
         self.latest_detection_timestamp = rclpy.time.Time(nanoseconds=timestamp_ms * 1e6)
-        annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
         
+        self.map_hand_to_robot_coords(result)
+
+        annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
+
         img = annotated_image  
         # Create ROS Image message
         image_msg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
@@ -126,10 +144,42 @@ class GestureTrackerNode(Node):
 
         self.image_pub.publish(image_msg)
         #print(image_msg)
-    
+
+    def map_hand_to_robot_coords(self, result: mp.tasks.vision.HandLandmarkerResult):
+        """
+        Map the normalized landmarks in [0 1] image height, width to the robot arm
+        """
+        if len(result.handedness) == 0:
+            return
+        img_bounds = np.array([[0, 1], [0, 1], [0, 1]])
+        robot_bounds = np.array([[0.15, 0.4], [-0.2, 0.2], [0.2, 0.2]])
+        # 8 is index finger tip. Get its XYZ coords
+        hand_pos = np.array([getattr(result.hand_landmarks[0][8], dim) for dim in ('x', 'y', 'z')])
+        robot_pos = np.empty(3)
+        
+        robot_pos = (hand_pos - img_bounds.min(axis=1)) * (robot_bounds[:, 1] - robot_bounds[:, 0]) / (img_bounds[:, 1] - img_bounds[:, 0]) + robot_bounds.min(axis=1)
+        self.hand_pos_pub.publish(Point(x=hand_pos[0], y=hand_pos[1], z=hand_pos[2]))
+
+        self.pose_msg.header.stamp = self.get_clock().now().to_msg()
+        self.pose_msg.pose.position.x = robot_pos[0]
+        self.pose_msg.pose.position.y = robot_pos[1]
+        self.pose_msg.pose.position.z = robot_pos[2]
+        self.robot_pose_pub.publish(self.pose_msg)
+
+        self.robot_control.update_target(robot_pos, self.target_orientation)
+        
+        
+    def control_loop(self):
+        res = self.robot_control.step()
+        if res is not None:
+            print(f"Error: {res}")
+            self.robot_control.stop()
+        # TODO pulbish joint angles
+
     def destroy_node(self, args):
-        arm_control.stop()
-        super().destroy_node(args)
+        self.robot_control.stop()
+        self.pipeline.stop()
+        super().destroy_node(*args)
 
 def main():
 
